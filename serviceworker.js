37ad3385ff90380b6d7be98ae49d90516ccb8a12
Lisa M. Venezia <leftylmv78@gmail.com>
2018-11-01 22:45:37 -0400

    chore: Beautify code files

--- serviceworker.js
+++ serviceworker.js
@@ -6,56 +6,54 @@
 console.log('Service Worker: Registered');
 
 self.addEventListener('install', function(event) {
-  console.log('We are here');
-  const cacheFiles = [
-      '/',
-      '/index.html',
-      '/restaurant.html',
-      '/css/styles.css',
-      '/js/dbhelper.js',
-      '/js/main.js',
-      '/js/restaurant_info.js',
-      '/serviceworker.js',
-      '/data/restaurants.json',
-      '/img/1.jpg',
-      '/img/2.jpg',
-      '/img/3.jpg',
-      '/img/4.jpg',
-      '/img/5.jpg',
-      '/img/6.jpg',
-      '/img/7.jpg',
-      '/img/8.jpg',
-      '/img/9.jpg',
-      '/img/10.jpg'
-  ];
+    console.log('We are here');
+    const cacheFiles = [
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/serviceworker.js',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+    ];
 
-  event.waitUntil(
-    caches.open('mws-restaurant-static-v1').then(function(cache) {
-      return cache.addAll(cacheFiles);
-    })
-  );
+    event.waitUntil(
+        caches.open('mws-restaurant-static-v1').then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('inside fetch');
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found ', event.request, ' in cache');
-          return response;
-      }
-      else {
-        console.log('Could not find ', event.request, ' in cache, FETCHING!');
-        return fetch(event.request)
-        .then(function(response) {
-            const clonedResponse = response.clone();
-            caches.open('mws-restaurant-static-v1').then(function(cache) {
-                cache.put(event.request, clonedResponse);
-            })
-            return response;
+    console.log('inside fetch');
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Found ', event.request, ' in cache');
+                return response;
+            } else {
+                console.log('Could not find ', event.request, ' in cache, FETCHING!');
+                return fetch(event.request)
+                    .then(function(response) {
+                        const clonedResponse = response.clone();
+                        caches.open('mws-restaurant-static-v1').then(function(cache) {
+                            cache.put(event.request, clonedResponse);
+                        })
+                        return response;
+                    })
+            }
         })
-      }
-    })
-  );
+    );
 });
-
