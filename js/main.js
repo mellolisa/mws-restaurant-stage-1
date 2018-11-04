a6f6bc7f455fd1fdbbb6395955aa1c73553cd5eb
Lisa M. Venezia <leftylmv78@gmail.com>
2018-11-04 09:53:17 -0500

    fix: update from h1 to h2 for restaurant name

--- js/main.js
+++ js/main.js
@@ -176,7 +176,7 @@ createRestaurantHTML = (restaurant) => {
     console.log(image);
     li.append(image);
 
-    const name = document.createElement('h1');
+    const name = document.createElement('h2');
     name.innerHTML = restaurant.name;
     li.append(name);
 
